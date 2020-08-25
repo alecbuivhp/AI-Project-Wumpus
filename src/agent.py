8ea42056d5216edd15c73af1242eaade1a6a39d0
kvmduc <kvmduc3@gmail.com>
2020-08-25 12:45:44 +0700

    update KB

--- src/agent.py
+++ src/agent.py
@@ -1,3 +1,6 @@
+from src.gamestate import *
+from src.kb import *
+
 class Agent:
     def __init__(self ,state, direction):
         self.current_state = state
@@ -27,5 +30,29 @@ class Agent:
 
     def turn_down(self):
         self.current_direction = "Down"
+
+    def shoot(self):
+        pass
+
+class Level_solver(Agent):
+    #FIX starting node
+    def __init__(self,world,starting_node): #Starting node = (1,1)
+        self.state = GameState(world)
+        self.state.add_state(Node(starting_node.row, starting_node.col))
+        self.agent = Agent(state = starting_node.name, direction = "Right")
+        self.KB = Knowledge_base()
+        self.KB.add(["~P" + str(starting_node.name)])
+        self.KB.add(["~W" + str(starting_node.name)])
+        self.direction = ['Up','Left','Down','Right']
+        self.exit = False
+        self.move = []
+        self.start_stench = False
+
+        def getAction(self,current_tile):
+            current_node = self.state.state[self.agent.current_state]
+            if current_node.name == starting_node and current_tile.getStench() and not current_tile.getBreeze():
+                self.start_stench = True
+                self.shoot()
+
 
 
